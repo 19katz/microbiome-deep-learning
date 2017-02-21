edd5a68a09cf31d2c38c81b2eca323d7305a864b
ngarud <nandita.garud@gmail.com>
2017-02-21 14:54:41 -0800

    grid search of parameter space

--- deep_learning_tutorial_sklearn.py
+++ deep_learning_tutorial_sklearn.py
@@ -27,43 +27,16 @@ def create_autoencoder():
     decoded = Dense(5952, activation='sigmoid')(encoded)
     # this model maps an input to its reconstruction
     autoencoder = Model(input=input_img, output=decoded)
-    autoencoder.compile(optimizer='adadelta', loss='mean_squared_error', metrics=['accuracy'])
+    autoencoder.compile(optimizer='adadelta', loss='mean_squared_error', metrics=['accuracy']) # NOT SURE IF THIS IS THE RIGHT METRIC (SHOULD IT BE neg_mean_squared_error? MSE?)
     return autoencoder
 
-def create_encoder():
-    input_img = Input(shape=(5952,))  
-    # create encoder
-    encoder = Model(input=input_img, output=encoded)
-    return encoder
-
-def create_decoder(autoencoder): # this doesn't work
-    # create decoder
-    encoding_dim = 5
-    # create a placeholder for an encoded (32-dimensional) input
-    encoded_input = Input(shape=(encoding_dim,))
-    # retrieve the last layer of the autoencoder model
-    decoder_layer = autoencoder.layers[-1]
-    # create the decoder model
-    decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
-    return decoder
-    
-
 autoencoder = KerasClassifier(build_fn=create_autoencoder, verbose=0)
-#encoder=create_encoder()
-#decoder=create_decoder(autoencoder)
-
-
-
 
 #################################
 # read in the data using pandas
 #################################
 filename="~/deep_learning/deep_learning_data/relative_abundance.txt.bz2"
 rel_abundance_matrix = pandas.read_csv(filename, sep = '\t', index_col = 0).T 
-
-# change to using scikit learn for 10-fold validation
-x_train=rel_abundance_matrix.values[:1300]
-x_test=rel_abundance_matrix.values[1300:]
 
 
 ###############
@@ -72,65 +45,19 @@ x_test=rel_abundance_matrix.values[1300:]
 
 epochs = [50, 100, 150]
 batches = [5, 10, 20]
-grid = GridSearchCV(estimator=autoencoder, param_grid=param_grid)  
+param_grid = dict(batch_size=batches, nb_epoch=epochs)
 
 grid = GridSearchCV(estimator=autoencoder, param_grid=param_grid)
-grid_result = grid.fit(x_train, x_train)
+grid_result = grid.fit(rel_abundance_matrix.values, rel_abundance_matrix.values)
 
-
-#################
-# Fit the model #
-#################
-
-x_train=rel_abundance_matrix.values[:1300]
-x_test=rel_abundance_matrix.values[1300:]   
-history = History() 
-autoencoder.fit(x_train, x_train,
-                nb_epoch=50,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test),
-                callbacks=[history])
-
-
-####################
-# Make predictions #
-####################
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
-
-#################
-# Plots         #
-#################
-
-# how does loss change with number of epochs?
-pylab.figure()
-pylab.plot(history.history['loss'])
-pylab.plot(history.history['val_loss'])
-pylab.title('model loss')
-pylab.ylabel('loss')
-pylab.xlabel('epoch')
-pylab.legend(['train', 'test'], loc='upper left')
-pylab.savefig(os.path.expanduser('~/deep_learning/deep_learning_analysis/epoch_vs_loss.pdf'), bbox_inches='tight')
-
-# how does the loss change as the fraction of data in the test vs training change?
-
-# how do the points before and after encoding compare (can we make a y=x line?)
-#matplotlib.rcParams['agg.path.chunksize'] = 10000 # might need this as there are too many points. 
-pylab.figure()
-num_data_pts=len(x_test.flatten())
-indexes=numpy.random.choice(num_data_pts,1000,replace=False)
-input_data=x_test.flatten()[indexes]
-decoded_data=decoded_imgs.flatten()[indexes]
-pylab.plot(input_data, decoded_data)
-pylab.title("input vs decoded data")
-pylab.ylabel("decoded data")
-pylab.xlabel("input data")
-pylab.savefig(os.path.expanduser('~/deep_learning/deep_learning_analysis/data_decoded.pdf'), bbox_inches='tight')
-
-# Plot alpha diversity before and after encoding
-
-# plot beta diveristy before and after encoding
-
-
+################################################
+# analyze which parameters gave the best score #
+################################################ 
+ 
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
