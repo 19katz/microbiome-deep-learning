1ebc2f757bc9d0942e068d7c8d6fc49448ed29c0
greensii <greensi@stanford.edu>
2017-04-21 12:59:30 -0700

    Update deep_learning_tutorial_sklearn_load_kmers.py

--- deep_learning_tutorial_sklearn_load_kmers.py
+++ deep_learning_tutorial_sklearn_load_kmers.py
@@ -5,9 +5,9 @@ import numpy as np
 from numpy import random
 import pandas as pd
 import os
-import pylab
+#import pylab
 from sklearn.feature_extraction.text import CountVectorizer
-from Bio import SeqIO
+#from Bio import SeqIO
 
 
 # using biopython, load the fastq file
@@ -17,14 +17,13 @@ from Bio import SeqIO
 #    print(sequence)
 #fasta_dict["SRR038746.22"].seq
 
-data=['NCATAGACATCAAGATTTTCTATAGTCTTTCCGTTACGCACCAACTTTATGCCACAT']
-v = CountVectorizer(analyzer = 'char', ngram_range = (7, 7), lowercase = True)
-v.fit_transform(data)
-v.get_feature_names()
-
+#data=['NCATAGACATCAAGATTTTCTATAGTCTTTCCGTTACGCACCAACTTTATGCCACAT']
+sequences_df=pandas.read_table("~/Documents/project_data/deepLearning/sample.fasta",index_col=0,header=None)
+sequences_df.columns=["sequence"]
+v = CountVectorizer(analyzer = 'char', ngram_range = (2, 5), lowercase = True)
 kmers_df = pd.DataFrame(
-    v.fit_transform(data).todense(),
-    index = data.index,
+    v.fit_transform(sequences_df['sequence']).todense(),
+    index = sequences_df.index,
     columns = v.get_feature_names()
 )
 
