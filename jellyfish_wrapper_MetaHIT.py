4ab365f682fdb63054b213471cdaf1a9e35b3a94
ngarud <nandita.garud@gmail.com>
2018-04-06 10:25:17 -0700

    creates bash scripts for running multiple jellyfish processes at once

---
+++ jellyfish_wrapper_MetaHIT.py
@@ -0,0 +1,65 @@
+import os.path
+import subprocess
+
+# For each batch, write a jellyfish command
+
+def write_command(batch, kmer_size):
+    command="while read file; do\n"
+    command += "echo $file\n"
+    command += "dir=/pollard/shattuck0/snayfach/metagenomes/MetaHIT/fastq/\n"
+    command += "jellyfish count <(zcat ${dir}/${file}_1.fastq.gz) <(zcat ${dir}/${file}_2.fastq.gz) /dev/fd/0 -m %s -s 100M -t 2 -C -F 2 -o ~/deep_learning_microbiome/data/%smers_jf/MetaHIT/${file}_%smer.jf \n" %(kmer_size,kmer_size, kmer_size) 
+    command += "jellyfish dump ~/deep_learning_microbiome/data/%smers_jf/MetaHIT/${file}_%smer.jf | grep '>' | gzip > ~/deep_learning_microbiome/data/%smers_jf/MetaHIT/${file}_%smer.gz\n" %(kmer_size,kmer_size, kmer_size, kmer_size)
+    command += "done <  ~/deep_learning_microbiome/tmp_intermediate_files/MetaHIT_batch_" + str(batch) + ".txt\n" 
+
+    bash_script=os.path.expanduser('~/deep_learning_microbiome/tmp_intermediate_files/') + 'MetaHIT_batch_' + str(batch) + '_bash_script.sh'
+    outFile=open(bash_script,'w')
+    outFile.write(command)
+    outFile.close()
+
+    return bash_script
+
+#########
+# main  #
+#########
+
+kmer_size=5
+number_of_processes=25
+
+list_of_samples=os.path.expanduser('~/deep_learning_microbiome/MetaHIT_accessions_only.txt')
+file=open(list_of_samples,'r')
+
+counter =0
+batch = 1
+list=os.path.expanduser('~/deep_learning_microbiome/tmp_intermediate_files/') + 'MetaHIT_batch_' + str(batch) + '.txt'  
+outFile=open(list,'w')
+
+
+command_file_inFN=os.path.expanduser('~/deep_learning_microbiome/tmp_intermediate_files/all_commands_MetaHIT_%smers.sh') % kmer_size
+command_file_inFile=open(command_file_inFN,'w')
+
+for sample in file:
+    if counter <number_of_processes:
+        outFile.write(sample) 
+        counter +=1
+    else:
+        bash_script=write_command(batch, kmer_size)
+        # execute command
+        command='nohup nice bash %s &' %(bash_script)
+        command_file_inFile.write(command +'\n')
+        
+        #update:
+        counter=1
+        batch +=1
+        list=os.path.expanduser('~/deep_learning_microbiome/tmp_intermediate_files/') + 'MetaHIT_batch_' + str(batch) + '.txt'  
+        outFile=open(list,'w')
+        outFile.write(sample)
+
+# last batch        
+outFile.close()
+bash_script=write_command(batch, kmer_size)
+# execute command
+command='nohup nice bash %s &' %(bash_script)
+command_file_inFile.write(command +'\n')
+#subprocess.call(command.split())
+
+
